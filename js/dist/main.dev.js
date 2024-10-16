e5575613d177e97c0ee1e19f4f95f2818dbf105b
dilnoza <dilnozaumurova7>
2024-10-16 11:27:24 +0500

    coffee

---
+++ js/dist/main.dev.js
@@ -0,0 +1,22 @@
+"use strict";
+
+$(function () {
+  $('.rev__slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 2000
+  });
+});
+$(function () {
+  $('.combo__slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 2000
+  });
+});
